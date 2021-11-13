2c6537f465338c6f4a9301fed9d179243c5c7b48
LuziaFernandes <87724388+LuziaFernandes@users.noreply.github.com>
2021-11-13 21:46:13 +0000

    Building search Engine that tracks what we are typing on the input

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './SearchEngine.css';
 
 export default function SearchEngine () {
+
+let [keyword, setKeyword] = useState("");
+
+function search(event){
+    event.preventDefault ();
+    alert (`${keyword}`);
+}
+
+function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+
+}
 
     return(
         <div className="SearchEngine">
 
 <div className="input-title">What word do you want to look up?</div>
 
-<form><input type="search" placeholder="Search a word ..."/></form>
+<form onSubmit={search}><input type="search" placeholder="Search a word ..." autoFocus={true} onChange={handleKeywordChange}/></form>
 
 </div>
        );
