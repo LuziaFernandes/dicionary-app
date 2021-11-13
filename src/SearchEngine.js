a26a7511832fb421474e11ce2a085c5e4f6d34f8
LuziaFernandes <87724388+LuziaFernandes@users.noreply.github.com>
2021-11-13 22:15:39 +0000

    api integration + import axios

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import './SearchEngine.css';
+import axios from "axios";
+
 
 export default function SearchEngine () {
 
 let [keyword, setKeyword] = useState("");
 
+function handleResponse (response) {
+console.log(response.data[0]);
+}
+
 function search(event){
     event.preventDefault ();
-    alert (`${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}` ;
+    axios.get(apiUrl).then(handleResponse);
+
 }
 
 function handleKeywordChange(event) {
