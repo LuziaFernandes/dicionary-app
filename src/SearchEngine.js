dc27f47af50923f2b86c70e04f089d931af45b3c
LuziaFernandes <87724388+LuziaFernandes@users.noreply.github.com>
2021-11-23 13:03:53 +0000

    Include pexels Api to showcase Photos  for each word

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './SearchEngine.css';
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 
 
@@ -8,16 +9,25 @@ export default function SearchEngine () {
 
 let [keyword, setKeyword] = useState("");
 let [results, setResults] = useState (null);
+let [photos, setPhotos] = useState(null);
 
-function handleResponse (response) {
+function handleDicitonaryResponse (response) {
 setResults(response.data[0]);
+}
+
+function handlePexelsResponse (response) {
+setPhotos(response.data.photos);
 }
 
 function search(event){
     event.preventDefault ();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}` ;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDicitonaryResponse);
+     
 
+    let apiKeyPhotos="563492ad6f917000010000016f0115c6c86644c18a14defcaba701cf";
+    let apiUrlPhotos= `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    axios.get(apiUrlPhotos, {headers: {"Authorization": `Bearer ${apiKeyPhotos}`}}).then(handlePexelsResponse);
 }
 
 function handleKeywordChange(event) {
@@ -36,6 +46,7 @@ function handleKeywordChange(event) {
 </form>
 
 <Results results={results}/>
+<Photos photos={photos}/>
 </div>
        );
 }
